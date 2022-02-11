259ee6cacd10e24c520995886b7200743a3ce995
Yugiponna <yugandharponna151193@gmail.com>
2022-02-11 22:07:34 +0530

    demo

---
+++ src/practise/DemoHello.java
@@ -0,0 +1,27 @@
+package practise;
+
+public class DemoHello {
+	public static void main (String[] args) {
+		System.out.println("This is Begining");
+		One y1 = new One();
+		y1.delhi();
+		y1.mumbai();
+		
+	}
+	
+	
+	}
+	class One{
+		public void delhi() {
+			System.out.println("This is Delhi");
+		}
+		
+		public void mumbai() {
+			System.out.println("This is Mumbai");
+		}
+		
+		
+	
+	
+
+}
